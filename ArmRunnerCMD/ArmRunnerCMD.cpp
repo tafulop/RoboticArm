350af467b68a30f2adef9ff55b514f02236baf3c
tafulop <tamas.flp@gmail.com>
2015-11-20 19:36:50 +0100

    Added dependencies.

---
+++ ArmRunnerCMD/ArmRunnerCMD.cpp
@@ -0,0 +1,28 @@
+// ArmRunnerCMD.cpp : Defines the entry point for the console application.
+
+#include "stdafx.h"
+#include "ArmRunner.h"
+#include "Calculation.h"
+#include "InverseKinematics.h"
+
+
+int main()
+{
+	using namespace RoboticArm;
+
+	// Initializing...
+	ArmRunner* AR = ArmRunner::getInstance();
+	AR->log->enableLogging(Logger::BOTH);
+	AR->log->printProgramStart(Logger::BOTH);
+
+	// Creating robotic arm
+	AR->initialize();
+	AR->operate();
+	//	PartContainer* PC = PartContainer::getInstance();
+
+	// prevent from auto-exiting
+	system("pause");
+
+	return 0;
+}
+
